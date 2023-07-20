b3901c6dfac7c9c64c7bfacf91753b06880de520
Loyalseason <easante658@gmail.com>
2023-07-20 12:10:50 +0000

    Update

---
+++ java/src/Practice/CircleClassOop.java
@@ -0,0 +1,42 @@
+package Practice;
+
+class Circle{
+	
+	// Properties
+	double radius;
+	
+	
+	
+	// Methods
+	double area(){
+	   double area = Math.PI * Math.sqrt(radius);
+	   return area;
+	}
+	
+	double perimeter(){
+		double perimeter = 2 * Math.PI * radius;
+		return perimeter;
+	}
+	
+	double circumference() {
+		return perimeter();
+	}
+}
+
+
+public class CircleClassOop {
+
+	public static void main(String[] args) {
+		
+		Circle c = new Circle();
+
+		c.radius = 5.9;
+	
+		System.out.println(c.area());
+		System.out.println(c.perimeter());
+		System.out.println(c.circumference());
+
+		
+	}
+	
+}
