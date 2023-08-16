d2c21850a3415952991497a97cde3f43e9f84ac1
Loyalseason <easante658@gmail.com>
2023-08-16 18:55:44 +0000

    Update

---
+++ java/src/Practice/InheritanceAndConstructors.java
@@ -0,0 +1,42 @@
+package Practice;
+
+class Rectangle{
+	int length;
+	int breadth;
+	Rectangle(){
+		length=breadth=1;
+	}
+	Rectangle(int l, int b){
+		length = l;
+		breadth = b;
+	}
+}
+
+class Cuboid extends Rectangle{
+	int height;
+	Cuboid(){
+		height = 1;
+	}
+	Cuboid(int h){
+		height = h;
+	}
+	Cuboid(int l, int h, int b){
+		super(l, h);
+		height = h;
+	}
+	
+	public int volume() {
+		return length * breadth * height;
+	}
+}
+
+public final class InheritanceAndConstructors {
+
+	public static void main(String[] args) {
+	
+		Cuboid c = new Cuboid(10);
+		System.out.println(c.volume());
+
+	}
+
+}
